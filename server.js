c2cdb1eb8cf00fd50a9c9d4457bf5763ae2a62e2
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-12 08:41:03 +0100

    Add snyk for version validation of all installed packages and add helmet

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -13,6 +14,7 @@ const server = app.listen(process.env.PORT || 8000, () => {
 });
 
 //middlewares
+app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
